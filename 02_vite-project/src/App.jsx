176125d0117c7ebc84f5bf7b8a2d092d99b9a14f
KIRANBADAKURAVA <kiranbadakurava@gmail.com>
2024-07-08 23:46:40 +0530

    learnings

---
+++ 02_vite-project/src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  function inc(){
+    console.log(count);
+    if(count<=19){
+    setCount(count+1)
+  }
+
+
+  }
+  function dec(){
+    console.log(count);
+    if(count>0){
+    setCount(count-1)
+  }
+
+
+  }
+
+  return (
+    <>
+    <h1>Hooks</h1>
+    <p>Minimum is '0' Maximum is '20' </p>
+    <button onClick={inc}>INC:{count}</button>
+    <button onClick={dec}>DEC:{count}</button>
+    </>
+  )
+}
+
+export default App
---
+++ vite-project_01/src/App.jsx
@@ -0,0 +1,15 @@
+import Test from "./Test"
+
+
+function App() {
+ 
+
+  return (
+    <>
+    <h1>React_01</h1>
+    <Test/>
+    </>
+  )
+}
+
+export default App
